44a88870e0541b5799da94babcca59a8023c1fdf
Chioma Ibe <chioma05@meta.com>
2025-06-30 18:29:30 -0700

    realised I've been exposing my API keys, updated withe environment variables

--- frontend/src/firebase/firebase.js
+++ frontend/src/firebase/firebase.js
@@ -2,13 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDvC0NRKaSYZRLgqjY97E-V_ULAjmWqrt8",
-  authDomain: "studdy-buddy-matcher.firebaseapp.com",
-  projectId: "studdy-buddy-matcher",
-  storageBucket: "studdy-buddy-matcher.firebasestorage.app",
-  messagingSenderId: "311990963979",
-  appId: "1:311990963979:web:64f86facf79f99fe23dc0e",
-  measurementId: "G-6J58F24SM7"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
