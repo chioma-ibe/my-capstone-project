25f4fb997a2b77606153a3b19beaa5f6911dd452
Chioma Ibe <chioma05@meta.com>
2025-07-01 18:14:42 -0700

    included port in cors config

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const userRoutes = require('./routes/users');
 const PORT = 3000;
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5174'],
+  origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
